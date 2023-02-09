5eb0eeb7005db5a9fb5dff30b2d2568b7b7a438f
DenBeef <91434725+DenBeef@users.noreply.github.com>
2023-02-09 10:30:06 +0000

    changed books.js and res_list.js key to id

--- pages/api/res_list.js
+++ pages/api/res_list.js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
     const resList = [
 
         {
-            key: 1,
+            id: 1,
             title: "CS50's Introduction to Computer Science",
             author: "David J. Malan",
             link: "https://www.edx.org/course/introduction-computer-science-harvardx-cs50x?utm_source=google&utm_campaign=19339199037&utm_medium=cpc&utm_term=harvard%20cs50%20online&hsa_acc=7245054034&hsa_cam=19339199037&hsa_grp=145482383700&hsa_ad=642397268536&hsa_src=g&hsa_tgt=kwd-422823376443&hsa_kw=harvard%20cs50%20online&hsa_mt=e&hsa_net=adwords&hsa_ver=3&gclid=CjwKCAiAleOeBhBdEiwAfgmXfxmOOSaE3ZZc_e8tzZyp5RAQteOmKqE20VsLHe9stEuoDMAAOTTzFxoCzpQQAvD_BwE",
@@ -11,14 +11,14 @@ export default function handler(req, res) {
         },
 
         {
-            key: 2,
+            id: 2,
             title: "W3 SCHOOL",
             author: "Refsnes Data",
             link: "https://www.w3schools.com/",
             image: "https://yt3.googleusercontent.com/dW6to0x5Crmeh7yi-YPLcQRqVrBtx2BSh8eoKTJbE8NbjloQ0sqlmdszIlxokJU_97-ndOt_=s900-c-k-c0x00ffffff-no-rj"
         },
         {
-            key: 2,
+            id: 3,
             title: "FreeCodeCamp",
             author: "Quincy Larson",
             link: "https://www.freecodecamp.org/",
