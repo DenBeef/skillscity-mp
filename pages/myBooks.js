06a771524c3d028b063f392eb358512a6d27c99a
DenBeef <91434725+DenBeef@users.noreply.github.com>
2023-02-10 20:07:20 +0000

    added h-screen to pages

--- pages/myBooks.js
+++ pages/myBooks.js
@@ -7,7 +7,7 @@ import Information from "@/components/home/bio";
 export default function myBooks() {
 
   return (
-    <div className="h-screen bg-[#EDEDE9]">
+    <div className="w-full h-screen bg-[#EDEDE9]">
       <NavBar />
       <Information text="I have read a number of books thus far on my journey to becoming a software engineer, and these are some of them." />
       <Grid dataSet={'books'} />
--- pages/resources.js
+++ pages/resources.js
@@ -4,7 +4,7 @@ import Information from "@/components/home/bio";
 
 export default function resources() {
     return (
-        <div className="h-screen bg-[#EDEDE9]">
+        <div className="w-full h-screen bg-[#EDEDE9]">
             <NavBar />
             <Information text="I have compiled a range of resources that I have utilized to further my knowledge and development as a software developer." />
             <Grid dataSet={'res_list'} />
