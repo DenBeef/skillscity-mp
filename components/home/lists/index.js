fac21f3a2cc28fdf658f51b2b764bc22663b1ec1
DenBeef <91434725+DenBeef@users.noreply.github.com>
2023-02-10 11:42:41 +0000

    Cleaned up the look of list component

--- components/home/lists/index.js
+++ components/home/lists/index.js
@@ -3,7 +3,7 @@ export default function Lists(props) {
         <div className="grid justify-items-center font-serif font-medium text-lg">
             <div className="w-1/2 text-center bg-[#D6CCC2] p-2 shadow-xl rounded-xl">
                 <p>{props.title}</p>
-                <ul className="mt-5 mb-5 grid grid-cols-3 w-full">
+                <ul className="mt-5 ml-10 mb-5 grid grid-cols-3 w-full text-left">
                     {props.arr.map((item) => {
                         return (
                             <li>▹ {item}</li>
