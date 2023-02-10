8b2801977b731a920165eb6e77e593c22f1c92d5
DenBeef <91434725+DenBeef@users.noreply.github.com>
2023-02-10 11:26:54 +0000

    Removed next.js Image component and replaced with standard HTML img, updated class css as well

--- components/shared/nav_bar/index.js
+++ components/shared/nav_bar/index.js
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import Name from "../../home/hero/index.js";
 import NavElement from "./nav_element.js";
-import profile from "../../../src/images/profile.png"
+
 
 export default function NavBar() {
 
     return (
         <div className="w-full flex justify-between bg-[#D6CCC2] p-5 shadow-xl">
-            <Image src={profile} className="object-none w-36 h-36 rounded-full custom-position mx-10" />
+            <img src="/profile.jpg" className="h-36 rounded-full mx-10" />
             <Name name="Denis Cleary" className="" />
             <nav className="py-2 mt-10">
                 <NavElement ahref="/" name="HOME" />
