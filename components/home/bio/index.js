a2c72040322c4e18825c7280eba9f4f8f355b9c9
DenBeef <91434725+DenBeef@users.noreply.github.com>
2023-02-10 11:53:19 +0000

    increased bio width

--- components/home/bio/index.js
+++ components/home/bio/index.js
@@ -4,7 +4,7 @@ export default function Information({ text }) {
     return (
         <div className="grid justify-items-center ">
             <div
-                className="w-1/2 flex text-center m-5 p-5 shadow-xl font-serif font-medium text-lg bg-[#D6CCC2] rounded-xl">
+                className="w-2/3 flex text-center m-5 p-5 shadow-xl font-serif font-medium text-lg bg-[#D6CCC2] rounded-xl">
                 {text}
             </div>
 
