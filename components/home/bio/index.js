bc8f44ef682f2f1c6b93a4656409f780f4dab081
DenBeef <91434725+DenBeef@users.noreply.github.com>
2023-02-20 16:54:12 +0000

    Moblie Responise clean up

--- components/home/bio/index.js
+++ components/home/bio/index.js
@@ -4,7 +4,7 @@ export default function Information({ text }) {
     return (
         <div className="grid justify-items-center ">
             <div
-                className="w-2/3 flex text-center m-5 p-5 shadow-xl font-serif font-medium text-lg bg-[#D6CCC2] rounded-xl">
+                className="w-full md:w-2/3 flex text-center m-5 p-5 shadow-xl font-serif font-medium text-lg bg-[#D6CCC2] rounded-xl">
                 {text}
             </div>
 
