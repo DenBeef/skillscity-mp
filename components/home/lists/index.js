cfd67213d385ddba01eca0eee36545cb1a89092d
DenBeef <91434725+DenBeef@users.noreply.github.com>
2023-02-20 17:51:41 +0000

    commit

--- components/home/lists/index.js
+++ components/home/lists/index.js
@@ -3,7 +3,7 @@ export default function Lists(props) {
         <div className="grid justify-items-center font-serif font-medium text-lg">
             <div className="md:w-1/2 text-center bg-[#D6CCC2] p-2 shadow-xl rounded-xl">
                 <p>{props.title}</p>
-                <ul className="mt-5 ml-4 mb-5 grid grid-cols-2 md:grid-cols-3 w-full px-4 text-left">
+                <ul className="mt-5 ml-4 mb-5 grid grid-cols-2 md:grid-cols-3 w-full text-left">
                     {props.arr.map((item, index) => {
                         return (
                             <li key={index} className="list-disc">{item}</li>
